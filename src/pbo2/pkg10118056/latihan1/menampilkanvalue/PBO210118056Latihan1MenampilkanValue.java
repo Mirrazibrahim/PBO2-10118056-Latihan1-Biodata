1d797123eba79fcda06eeb1750018254d01c6a1f
Mirrazibrahim <55575435+Mirrazibrahim@users.noreply.github.com>
2019-09-26 12:58:35 +0700

    Add files via upload

---
+++ src/pbo2/pkg10118056/latihan1/menampilkanvalue/PBO210118056Latihan1MenampilkanValue.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118056.latihan1.menampilkanvalue;
+
+/**
+ *
+ * @author 
+ * NAMA     : Mirrazibrahim
+ * KELAS    : IF-2
+ * NIM      : 10118056
+ * Deskripsi Program : Program ini berisi program untuk menampilkan
+ * biodata penulis
+ */
+public class PBO210118056Latihan1MenampilkanValue {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "--------------------");
+        System.out.println("NAMA\t\t: Mirraz Ibrahim");
+        System.out.println("ALAMAT\t\t: Jalan Antapani no 89");
+        System.out.println("TEMPAT LAHIR\t: Cirebon ");
+        System.out.println("TANGGAL LAHIR\t: 25 Oktober 2000");
+        System.out.println("--------------------");
+        System.out.println("HOBI\t\t: Foto Foto");
+        System.out.println("MAKEs\t\t: Ayam Goreng");
+        System.out.println("MIKES\t\t: Teh Manis");
+    }
+    
+}
